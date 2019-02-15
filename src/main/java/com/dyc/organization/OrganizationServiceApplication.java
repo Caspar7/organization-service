5af4358f44bebfe48bacb0846a19fbb37308a7d9
Yunchao Du <ycdu@thoughtworks.com>
2019-02-15 14:14:50 +0800

    update

--- src/main/java/com/dyc/organization/OrganizationServiceApplication.java
+++ src/main/java/com/dyc/organization/OrganizationServiceApplication.java
@@ -2,9 +2,13 @@ package com.dyc.organization;
 
 import com.dyc.organization.model.Organization;
 import com.dyc.organization.repository.OrganizationRepository;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -17,7 +21,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableSwagger2
+@EnableSwagger2@EnableHystrix
+@EnableCircuitBreaker
 public class OrganizationServiceApplication {
 
     public static void main(String[] args) {
@@ -33,7 +38,15 @@ public class OrganizationServiceApplication {
                 .build()
                 .apiInfo(new ApiInfoBuilder().version("1.0").title("Organization API").description("Documentation Organization API v1.0").build());
     }
-
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
     @Bean
     OrganizationRepository repository() {
         OrganizationRepository repository = new OrganizationRepository();
